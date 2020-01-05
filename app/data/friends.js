1e180633b739675cd420583188d9514ec5c26729
Andre <andre.morrall@icloud.com>
2020-01-04 20:34:54 -0600

    Updated with more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,70 @@
 
 
-var friends = [
+var friendArray = [
     {
-        name: 'Dallas',
+        name: 'Josh',
         photo: 'https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
         scores: [
             "5",
-            "1",
-            "4",
-            "4",
-            "5",
-            "1",
+            "3",
             "2",
-            "5",
             "4",
-            "1"
+            "3",
+            "1",
+            "1",
+            "4",
+            "4",
+            "2"
         ]
     },
     {
         name: 'Christina',
         photo: 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
         scores: [
-            '1',
-            '4',
-            '5',
-            '3',
-            '5',
-            '2',
-            '4',
-            '1',
-            '5',
-            '3'
+            "3",
+            "1",
+            "3",
+            "2",
+            "3",
+            "4",
+            "2",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: 'Brittany',
+        photo: 'https://images.pexels.com/photos/1139247/pexels-photo-1139247.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        scores: [
+            "1",
+            "5",
+            "2",
+            "3",
+            "2",
+            "1",
+            "1",
+            "4",
+            "5",
+            "3"
+        ]
+    },
+    {
+        name: 'Justin',
+        photo: 'https://images.pexels.com/photos/2381722/pexels-photo-2381722.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        scores: [
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "5",
+            "3",
+            "4",
+            "1",
+            "2"
         ]
     }
 ];
 
-module.exports = friends;
+module.exports = friendArray;
