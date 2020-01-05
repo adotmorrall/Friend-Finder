348f2b607061856ece1876ed61f69253dfbd6273
Andre <andre.morrall@icloud.com>
2020-01-04 19:44:41 -0600

    Manually added friends for testing purposes

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+
+
+var friends = [
+    {
+        name: 'Dallas',
+        photo: 'https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: 'Christina',
+        photo: 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        scores: [
+            '1',
+            '4',
+            '5',
+            '3',
+            '5',
+            '2',
+            '4',
+            '1',
+            '5',
+            '3'
+        ]
+    }
+];
