fea57d07ded10795b9c175ac5b368a1095391d35
Andre <andre.morrall@icloud.com>
2020-01-05 15:39:39 -0600

    Math logic to find most compatible friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,18 +9,42 @@ module.exports = function (app) {
 
     // Add new friend
     app.post('/api/friends', function (req, res) {
+
+        // Will hold the match for the best friend
+        var bestFriend = {
+            name: '',
+            photo: '',
+            friendDiff: 1000
+        };
+
+        // Making sure the new friend info is captured
         var newFriend = req.body;
         console.log(newFriend);
 
-        // Convert user scares into an array
-        newFriend.scores = parseInt(newFriend.scores);
-        for (var i = 0; i < newFriend.scores; i++) {
-            console.log(newFriend.scores[i]);
+        // Using this variable to calculate diff b/w newFriend and friends in the database
+        var matchIndex = 0;
+        var minDiff = 50;
+
+        // Looping through friends in my 'database'
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i].name);
+            var totalDiff = 0;
+
+            // Created to loop through friend scores
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                // Math to compare new friend score to existing friends
+                var diff = Math.abs(parseInt(newFriend.scores[j]) - parseInt(friends[i].scores[j]));
+                totalDiff += diff;
+
+                if (totalDiff < minDiff) {
+                    matchIndex = i;
+                    minDiff = totalDiff;
+                    console.log(bestFriend);
+                }
+            }
         }
-
-
         // Add friend scores
-        // friends.push(newFriend);
-        // res.json(newFriend);
+        friends.push(newFriend);
+        res.json(newFriend[matchIndex]);
     });
 }
