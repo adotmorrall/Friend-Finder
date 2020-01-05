d17d45bd4a94b9de71533b1eac109969421e34be
Andre <andre.morrall@icloud.com>
2020-01-04 19:53:31 -0600

    Removed routes & testing information

--- server.js
+++ server.js
@@ -2,36 +2,20 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Friends, How many of us have them? - Whodini
-var friends = [
-    {
-        name: 'Dallas',
-        photo: 'https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-        scores: [
-            "5",
-            "1",
-            "4",
-            "4",
-            "5",
-            "1",
-            "2",
-            "5",
-            "4",
-            "1"
-        ]
-    },
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
-];
+// Code from earlier testing
 
-
-// Move routes to the appropriate files after testing
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './app/public/home.html'));
+/*
+// GitHub Repo
+app.get('/github', function (req, res) {
+    res.sendFile(path.join(__dirname, 'https://github.com/adotmorrall/Friend-Finder.git'));
 });
 
 // Display friends
@@ -39,7 +23,18 @@ app.get('/api/friends', function (req, res) {
     return res.json(friends);
 });
 
+// Add new friend
+app.post('/api/friends', function (req, res) {
+    var newFriend = req.body;
+
+    newFriend.name = newFriend.name.replace(/\s+/g, "");
+    console.log(newFriend);
+    friends.push(newFriend);
+    res.json(newFriend);
+});
+
 // Start server to begin listening
 app.listen(PORT, function () {
     console.log('App listening on http://localhost:' + PORT);
 });
+*/
