00acecf4a77acca39b8509ee85d64b487f3b7cbc
Andre <andre.morrall@icloud.com>
2020-01-04 20:36:18 -0600

    Complete routing to webpages

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+// Routing
+module.exports = function (app) {
+
+    // Homepage
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+
+    // Survey
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/survey.html'));
+    });
+
+    // If no matching route is found default to home
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+
+};
