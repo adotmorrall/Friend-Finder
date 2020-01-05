403954d888f4b55388f2db1231e8b816d49119e4
Andre <andre.morrall@icloud.com>
2020-01-04 20:39:42 -0600

    Clean-up code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -10,31 +9,7 @@ app.use(express.json());
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
-// Code from earlier testing
-
-/*
-// GitHub Repo
-app.get('/github', function (req, res) {
-    res.sendFile(path.join(__dirname, 'https://github.com/adotmorrall/Friend-Finder.git'));
-});
-
-// Display friends
-app.get('/api/friends', function (req, res) {
-    return res.json(friends);
-});
-
-// Add new friend
-app.post('/api/friends', function (req, res) {
-    var newFriend = req.body;
-
-    newFriend.name = newFriend.name.replace(/\s+/g, "");
-    console.log(newFriend);
-    friends.push(newFriend);
-    res.json(newFriend);
-});
-
 // Start server to begin listening
 app.listen(PORT, function () {
     console.log('App listening on http://localhost:' + PORT);
 });
-*/
